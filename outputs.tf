224e920aa3779550f0077371444ff17c48583142
bgants <bobgants@gmail.com>
2025-04-05 10:52:39 -0400

    add subnet_id to outputs.tf file

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "aws_instance" {
   description = "The entire hello world instance object"
   value      = aws_instance.hello_world
 }
+
+output "aws_instance_subnet_id {
+  description = "The subnet ID of the hello world instance"
+  value      = aws_instance.hello_world.subnet_id
+}
