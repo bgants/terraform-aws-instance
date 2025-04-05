d8ef2a4d325d1b3e98c99c543fe6e6894ff5dc75
bgants <bobgants@gmail.com>
2025-04-05 10:38:52 -0400

    Fix findings in tf file

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "aws_instance_arn" {
-    description = "The ARN of the hello world instance"
-    values = aws_instance.hello_world.arn
+  description = "The ARN of the hello world instance"
+  value      = aws_instance.hello_world.arn
 }
 
 output "aws_instance_ip" {
-    description = "The public IP of the hello world instance"
-    values = aws_instance.hello_world.public_ip
+  description = "The public IP of the hello world instance"
+  value      = aws_instance.hello_world.public_ip
 }
 
 output "aws_instance" {
-    description = "The entire hello world instance object"
-    values = aws_instance.hello_world
+  description = "The entire hello world instance object"
+  value      = aws_instance.hello_world
 }
