450926745169a393aec794443a1e534388002964
bgants <bobgants@gmail.com>
2025-04-06 14:45:40 -0400

    Update variables.tf and main.tf with variables

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "hello_world" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = va.ami
   subnet_id     = var.subnet_id
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,18 @@ variable "subnet_id" {
   type        = string
   default     = "subnet-29876e4d"
 }
+variable "ami" {
+  type        = string
+  description = "The Amazon Machine Image to use when launching the EC2 Instance."
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "The ID of the Subnet to launch the instance into."
+}
+
+variable "instance_type" {
+  type        = string
+  description = "The type of instance to launch."
+  default     = "t3.micro"
+}
